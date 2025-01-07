581214bff6440e25234491435358cd93476e0843
Kevin Sylvestre <kevin@ksylvest.com>
2025-01-07 15:03:40 -0800

    Support for draw via cli

--- spec/mupdf/document_spec.rb
+++ spec/mupdf/document_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe MuPDF::Document do
     it { expect(pages).to all(be_a(MuPDF::Page)) }
     it { expect(pages.size).to eq(2) }
   end
+
+  describe '#draw' do
+    subject(:draw) { document.draw(path: tempfile.path, page: 2, format: 'png') }
+
+    let(:tempfile) { Tempfile.new(['mupdf', '.png']) }
+
+    around do |example|
+      example.run
+    ensure
+      tempfile.close
+      tempfile.unlink
+    end
+
+    it 'draws' do
+      draw
+      expect(File.size(tempfile.path)).to be > 0
+    end
+  end
 end
