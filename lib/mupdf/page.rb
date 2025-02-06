a2190c6312efc21d991ae07dd821253331ab28db
Kevin Sylvestre <kevin@ksylvest.com>
2025-02-06 10:50:26 -0800

    Further fixes for missing boxes

--- lib/mupdf/page.rb
+++ lib/mupdf/page.rb
@@ -28,62 +28,72 @@ module MuPDF
 
     # @param text [String]
     #
-    # @return [MuPDF::Box]
+    # @return [MuPDF::Box, nil]
     def self.parse_media_box(text)
       match = MEDIA_BOX_REGEX.match(text)
+      return unless match
+
       MuPDF::Box.parse(match[:content], kind: :media)
     end
 
     # @param text [String]
     #
-    # @return [MuPDF::Box]
+    # @return [MuPDF::Box, nil]
     def self.parse_crop_box(text)
       match = CROP_BOX_REGEX.match(text)
+      return unless match
+
       MuPDF::Box.parse(match[:content], kind: :crop)
     end
 
     # @param text [String]
     #
-    # @return [MuPDF::Box]
+    # @return [MuPDF::Box, nil]
     def self.parse_art_box(text)
       match = ART_BOX_REGEX.match(text)
+      return unless match
+
       MuPDF::Box.parse(match[:content], kind: :art)
     end
 
     # @param text [String]
     #
-    # @return [MuPDF::Box]
+    # @return [MuPDF::Box, nil]
     def self.parse_bleed_box(text)
       match = BLEED_BOX_REGEX.match(text)
+      return unless match
+
       MuPDF::Box.parse(match[:content], kind: :bleed)
     end
 
     # @param text [String]
     #
-    # @return [MuPDF::Box]
+    # @return [MuPDF::Box, nil]
     def self.parse_trim_box(text)
       match = TRIM_BOX_REGEX.match(text)
+      return unless match
+
       MuPDF::Box.parse(match[:content], kind: :trim)
     end
 
     # @!attribute media_box
-    #   @return [MuPDFBox]
+    #   @return [MuPDFBox, nil]
     attr_accessor :media_box
 
     # @!attribute crop_box
-    #   @return [MuPDFBox]
+    #   @return [MuPDFBox, nil]
     attr_accessor :crop_box
 
     # @!attribute art_box
-    #   @return [MuPDFBox]
+    #   @return [MuPDFBox, nil]
     attr_accessor :art_box
 
     # @!attribute bleed_box
-    #   @return [MuPDFBox]
+    #   @return [MuPDFBox, nil]
     attr_accessor :bleed_box
 
     # @!attribute trim_box
-    #   @return [MuPDFBox]
+    #   @return [MuPDFBox, nil]
     attr_accessor :trim_box
 
     # @!attribute pagenum
